de0faa0c9203c71c8461107cab697c51b4271358
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-01-22 10:56:40 -0600

    Basic Js Variable Types
    Playing around with basic variable types in JS, mostly refresher expect for a few key difference in java. defining variables is more similar to python in that you don't have to be super explicit about type.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,32 @@
-console.log('Hello World!!!');
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+
+console.log(firstName,lastName,'is',age + '.');
+console.log(fullAge);
+
+var fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+console.log(fullName,'is',age+'.');
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+
+//cannot start a variable name with a number
+//'_' AND '$' can be the first character
+//generally use a letter though as first character
+//name variables usefully
+//cannot use reserved keywords(Delete, if, else, etc.)
+//use camelCase notation when naming
+//var 3years = 3;
+
