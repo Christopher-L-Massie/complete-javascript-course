046ae79e3970ba55685f6d56f9717d6c60baec8b
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-01-27 10:40:44 -0600

    This keyword (important)
    learned about the intricacies of the 'THIS' keyword in javascript. some interesting things to be noted in this about how scope is treated in javascript. will be good to come back to

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,6 +1,6 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
-
+/*
 calculateAge(1990);
 
 //functions
@@ -29,6 +29,26 @@ function foo(){
 foo();
 console.log(age);
 
+//scoping chain
+//lexically
+*/
+/*
+
+var a = 'Hello!';
+first();
+
+function first(){
+    var b = 'Hi!';
+    second();
+    
+    function second(){
+    var c = 'Hey!';
+    console.log(a + b + c);
+    }
+    
+}
+
+*/
 
 
 
@@ -87,6 +107,97 @@ function third() {
 
 ///////////////////////////////////////
 // Lecture: The this keyword
+
+//console.log(this);
+
+calculateAge(1985);
+
+
+//in a regular function call the this variable attaches to the global object (this refering to the global object)
+function calculateAge(year) {
+    console.log(2020 - year);
+    console.log(this);
+}
+
+//in a method call from within an object the this variable points the object that it is contained in rather then the global object
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function(){
+        console.log(this);
+        console.log(2020 - this.yearOfBirth);
+        
+        //technically this is a regular function call so 'this' points to global object, not the created object
+        /*
+        function innerFunction(){
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+}
+
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+//function borrowing
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
