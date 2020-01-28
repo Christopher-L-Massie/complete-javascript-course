b01f8251f111ca6b626239adeeecab740a0f0fc1
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-01-28 15:58:05 -0600

    Events and Event Handling
    learned about event handling and a lot of manipulating html with javascript which was fun!

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,30 +9,42 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6 + 1)
-
-//can only set text with below and not manipulate html
-document.querySelector("#current-" + activePlayer).textContent = dice;
-//below is how you change the html of the webpage using javascript :)
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6 + 1);
+    
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    
+    //3. Update the round score If the rolled number was NOT a 1
+    
+});
 
 
 
-
-
+//can only set text with below and not manipulate html
+//document.querySelector("#current-" + activePlayer).textContent = dice;
+//below is how you change the html of the webpage using javascript :)
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+//var x = document.querySelector('#score-0').textContent;
 
 
 
