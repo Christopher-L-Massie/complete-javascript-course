832a3d81054804e3773af950a3dcb6024a8e3fe0
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-06 15:53:27 -0600

    Object.create
    learned about object.create and other ways to fill an object with values.

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,6 +1,6 @@
 /* Function Constructor
 */
-
+/*
 var john = {
     name:'John',
     yearOfBirth: 1990,
@@ -32,6 +32,71 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+
+//Object.create
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto,
+{
+    name: { value : 'Jane'},
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer'}
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
