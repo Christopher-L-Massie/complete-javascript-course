f6b6794a526b1a9496ffeb4db06bd6b56e538313
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-07 11:41:45 -0600

    Functions returning Functions
    I understand how this works but I'm struggling to think of a time I would ever use it over something else. Maybe i'm just not thinking about the right problems.

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -95,6 +95,7 @@ console.log(age);
 console.log(obj.city);
 */
 
+/*
 var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn){
@@ -129,6 +130,37 @@ var rates = arrayCalc(ages, maxHeartRate);
 
 console.log(ages);
 console.log(fullAges);
+*/
+
+/***********************************
+* Lecture: Functions returning functions
+**********************************/
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name){
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name){
+            console.log('Hello ' + name + ', what do you do?')
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('Mary');
+designerQuestion('John');
+designerQuestion('Mark');
+teacherQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
 
 
 
