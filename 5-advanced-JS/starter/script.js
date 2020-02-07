aceec109ed9c330358353e9261c85a7bb7b46fdf
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-07 12:21:57 -0600

    Closures
    learned about Closures in javascript they're actually pretty cool

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -176,6 +176,7 @@ function game() {
 game();
 */
 
+/*
 //IIFE
 (function () {
     var score = Math.random() * 10;
@@ -190,15 +191,66 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 - goodLuck);
 })(5);
+*/
 
+/*******
+* Lecture : Closures
+*********/
 
+//CLOSURE DEFINITION
+/*
+An inner function always has access to the variables and parameters of its outer function,
+even after the outer function has returned.
+*/
 
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth){
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
 
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
 
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+//retirement(66)(1990);
 
+function interviewQuestion(job) {
+    return function(name){
+        if (job === 'designer'){
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher'){
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?')
+        }
+    }
+}
 
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name){
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name){
+            console.log('Hello ' + name + ', what do you do?')
+        }
+    }
+}
+*/
 
-
+interviewQuestion('teacher')('John');
 
 
 
