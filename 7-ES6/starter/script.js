1f17a1b75c5fcb8f3b58ec5d775115412a35a20c
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-20 16:57:32 -0600

    ES6 Subclasses
    I'm really glad that ES6 has classes and subclasses because I really enjoy working with them in java!

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -487,6 +487,7 @@ console.log(question.get(ans === question.get('correct')));
 //////////////
 // Lecture: Classes
 
+/*
 //ES5
 var Person5 = function(name, yearOfBirth, job) {
     this.name = name;
@@ -522,6 +523,83 @@ class Person6 {
 const john6 = new Person6('John', 1990, 'teacher');
 
 Person6.greeting();
+*/
+
+/////////////////////////////////////////////////
+/// Lecture: Classes and subclasses
+
+//ES5
+var Person5 = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function () {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+
+var Athelete5 = function(name, yearOfBirth, job, olympicGames, medals){
+    Person5.call(this, name, yearOfBirth, job);
+    this.olympicGames;
+    this.mdeals = medals;
+}
+
+
+
+Athelete5.prototype = Object.create(Person5.prototype);
+
+Athelete5.prototype.wonMedal = function(){
+    this.medals++;
+    console.log(this.medals);
+}
+
+var johnAthelete5 = new Athelete5('John', 1990, 'swimmer', 3, 10);
+
+johnAthelete5.calculateAge();
+
+//ES6
+class Person6 {
+    constructor (name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+    
+    calculateAge() {
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+}
+
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals){
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+    
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+
+const johnAthelete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
+
+johnAthelete6.wonMedal();
+johnAthelete6.calculateAge();
+
+
+
+
+
+
+
+
+
+
 
 
 
